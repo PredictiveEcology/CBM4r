1d1acea09b2c980bc807e1f47be3cce11b726abb
Susan Murray <murray.e.susan@gmail.com>
2026-03-25 12:08:54 -0700

    add cbm4_data directory exists check

--- R/cbm4_results_query.R
+++ R/cbm4_results_query.R
@@ -9,6 +9,9 @@
 #' @export
 cbm4_results_query <- function(cbm4_data, query){
 
+  if (length(cbm4_data) == 0) stop("cbm4_data invalid")
+  if (!file.exists(cbm4_data)) stop("cbm4_data not found: ", cbm4_data)
+
   results_processor <- reticulate::import(
     "cbm4.app.spatial.results.sql_results_processor"
   )$SQLResultsProcessor$for_simulation(cbm4_data)
