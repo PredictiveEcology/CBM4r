afdf3dd6bd743a6dc82108e6d8c76cdf8299410b
Susan Murray <murray.e.susan@gmail.com>
2026-03-25 15:44:24 -0700

    results update: SW and HW pools merged

--- inst/results/pools_by_pixel.sql
+++ inst/results/pools_by_pixel.sql
@@ -6,16 +6,16 @@ SELECT
   b.raster_index,
   b.chunk_index,
   SUM("pools.Input"                   * ("inventory.area" * cohort_proportion)) AS Input,
-  SUM("pools.SoftwoodMerch"           * ("inventory.area" * cohort_proportion)) AS SoftwoodMerch,
-  SUM("pools.SoftwoodFoliage"         * ("inventory.area" * cohort_proportion)) AS SoftwoodFoliage,
-  SUM("pools.SoftwoodOther"           * ("inventory.area" * cohort_proportion)) AS SoftwoodOther,
-  SUM("pools.SoftwoodCoarseRoots"     * ("inventory.area" * cohort_proportion)) AS SoftwoodCoarseRoots,
-  SUM("pools.SoftwoodFineRoots"       * ("inventory.area" * cohort_proportion)) AS SoftwoodFineRoots,
-  SUM("pools.HardwoodMerch"           * ("inventory.area" * cohort_proportion)) AS HardwoodMerch,
-  SUM("pools.HardwoodFoliage"         * ("inventory.area" * cohort_proportion)) AS HardwoodFoliage,
-  SUM("pools.HardwoodOther"           * ("inventory.area" * cohort_proportion)) AS HardwoodOther,
-  SUM("pools.HardwoodCoarseRoots"     * ("inventory.area" * cohort_proportion)) AS HardwoodCoarseRoots,
-  SUM("pools.HardwoodFineRoots"       * ("inventory.area" * cohort_proportion)) AS HardwoodFineRoots,
+  SUM("pools.SoftwoodMerch"           * ("inventory.area" * cohort_proportion)) +
+  SUM("pools.HardwoodMerch"           * ("inventory.area" * cohort_proportion)) AS Merch,
+  SUM("pools.SoftwoodFoliage"         * ("inventory.area" * cohort_proportion)) +
+  SUM("pools.HardwoodFoliage"         * ("inventory.area" * cohort_proportion)) AS Foliage,
+  SUM("pools.SoftwoodOther"           * ("inventory.area" * cohort_proportion)) +
+  SUM("pools.HardwoodOther"           * ("inventory.area" * cohort_proportion)) AS Other,
+  SUM("pools.SoftwoodCoarseRoots"     * ("inventory.area" * cohort_proportion)) +
+  SUM("pools.HardwoodCoarseRoots"     * ("inventory.area" * cohort_proportion)) AS CoarseRoots,
+  SUM("pools.SoftwoodFineRoots"       * ("inventory.area" * cohort_proportion)) +
+  SUM("pools.HardwoodFineRoots"       * ("inventory.area" * cohort_proportion)) AS FineRoots,
   SUM("pools.AboveGroundVeryFastSoil" * ("inventory.area" * cohort_proportion)) AS AboveGroundVeryFastSoil,
   SUM("pools.BelowGroundVeryFastSoil" * ("inventory.area" * cohort_proportion)) AS BelowGroundVeryFastSoil,
   SUM("pools.AboveGroundFastSoil"     * ("inventory.area" * cohort_proportion)) AS AboveGroundFastSoil,
@@ -23,10 +23,10 @@ SELECT
   SUM("pools.MediumSoil"              * ("inventory.area" * cohort_proportion)) AS MediumSoil,
   SUM("pools.AboveGroundSlowSoil"     * ("inventory.area" * cohort_proportion)) AS AboveGroundSlowSoil,
   SUM("pools.BelowGroundSlowSoil"     * ("inventory.area" * cohort_proportion)) AS BelowGroundSlowSoil,
-  SUM("pools.SoftwoodStemSnag"        * ("inventory.area" * cohort_proportion)) AS SoftwoodStemSnag,
-  SUM("pools.SoftwoodBranchSnag"      * ("inventory.area" * cohort_proportion)) AS SoftwoodBranchSnag,
-  SUM("pools.HardwoodStemSnag"        * ("inventory.area" * cohort_proportion)) AS HardwoodStemSnag,
-  SUM("pools.HardwoodBranchSnag"      * ("inventory.area" * cohort_proportion)) AS HardwoodBranchSnag,
+  SUM("pools.SoftwoodStemSnag"        * ("inventory.area" * cohort_proportion)) +
+  SUM("pools.HardwoodStemSnag"        * ("inventory.area" * cohort_proportion)) AS StemSnag,
+  SUM("pools.SoftwoodBranchSnag"      * ("inventory.area" * cohort_proportion)) +
+  SUM("pools.HardwoodBranchSnag"      * ("inventory.area" * cohort_proportion)) AS BranchSnag,
   SUM("pools.CO2"                     * ("inventory.area" * cohort_proportion)) AS CO2,
   SUM("pools.CH4"                     * ("inventory.area" * cohort_proportion)) AS CH4,
   SUM("pools.CO"                      * ("inventory.area" * cohort_proportion)) AS CO,
--- inst/results/pools_by_timestep.sql
+++ inst/results/pools_by_timestep.sql
@@ -4,16 +4,16 @@
 SELECT
   timestep,
   SUM("pools.Input"                   * ("inventory.area" * cohort_proportion)) AS Input,
-  SUM("pools.SoftwoodMerch"           * ("inventory.area" * cohort_proportion)) AS SoftwoodMerch,
-  SUM("pools.SoftwoodFoliage"         * ("inventory.area" * cohort_proportion)) AS SoftwoodFoliage,
-  SUM("pools.SoftwoodOther"           * ("inventory.area" * cohort_proportion)) AS SoftwoodOther,
-  SUM("pools.SoftwoodCoarseRoots"     * ("inventory.area" * cohort_proportion)) AS SoftwoodCoarseRoots,
-  SUM("pools.SoftwoodFineRoots"       * ("inventory.area" * cohort_proportion)) AS SoftwoodFineRoots,
-  SUM("pools.HardwoodMerch"           * ("inventory.area" * cohort_proportion)) AS HardwoodMerch,
-  SUM("pools.HardwoodFoliage"         * ("inventory.area" * cohort_proportion)) AS HardwoodFoliage,
-  SUM("pools.HardwoodOther"           * ("inventory.area" * cohort_proportion)) AS HardwoodOther,
-  SUM("pools.HardwoodCoarseRoots"     * ("inventory.area" * cohort_proportion)) AS HardwoodCoarseRoots,
-  SUM("pools.HardwoodFineRoots"       * ("inventory.area" * cohort_proportion)) AS HardwoodFineRoots,
+  SUM("pools.SoftwoodMerch"           * ("inventory.area" * cohort_proportion)) +
+  SUM("pools.HardwoodMerch"           * ("inventory.area" * cohort_proportion)) AS Merch,
+  SUM("pools.SoftwoodFoliage"         * ("inventory.area" * cohort_proportion)) +
+  SUM("pools.HardwoodFoliage"         * ("inventory.area" * cohort_proportion)) AS Foliage,
+  SUM("pools.SoftwoodOther"           * ("inventory.area" * cohort_proportion)) +
+  SUM("pools.HardwoodOther"           * ("inventory.area" * cohort_proportion)) AS Other,
+  SUM("pools.SoftwoodCoarseRoots"     * ("inventory.area" * cohort_proportion)) +
+  SUM("pools.HardwoodCoarseRoots"     * ("inventory.area" * cohort_proportion)) AS CoarseRoots,
+  SUM("pools.SoftwoodFineRoots"       * ("inventory.area" * cohort_proportion)) +
+  SUM("pools.HardwoodFineRoots"       * ("inventory.area" * cohort_proportion)) AS FineRoots,
   SUM("pools.AboveGroundVeryFastSoil" * ("inventory.area" * cohort_proportion)) AS AboveGroundVeryFastSoil,
   SUM("pools.BelowGroundVeryFastSoil" * ("inventory.area" * cohort_proportion)) AS BelowGroundVeryFastSoil,
   SUM("pools.AboveGroundFastSoil"     * ("inventory.area" * cohort_proportion)) AS AboveGroundFastSoil,
@@ -21,15 +21,15 @@ SELECT
   SUM("pools.MediumSoil"              * ("inventory.area" * cohort_proportion)) AS MediumSoil,
   SUM("pools.AboveGroundSlowSoil"     * ("inventory.area" * cohort_proportion)) AS AboveGroundSlowSoil,
   SUM("pools.BelowGroundSlowSoil"     * ("inventory.area" * cohort_proportion)) AS BelowGroundSlowSoil,
-  SUM("pools.SoftwoodStemSnag"        * ("inventory.area" * cohort_proportion)) AS SoftwoodStemSnag,
-  SUM("pools.SoftwoodBranchSnag"      * ("inventory.area" * cohort_proportion)) AS SoftwoodBranchSnag,
-  SUM("pools.HardwoodStemSnag"        * ("inventory.area" * cohort_proportion)) AS HardwoodStemSnag,
-  SUM("pools.HardwoodBranchSnag"      * ("inventory.area" * cohort_proportion)) AS HardwoodBranchSnag,
+  SUM("pools.SoftwoodStemSnag"        * ("inventory.area" * cohort_proportion)) +
+  SUM("pools.HardwoodStemSnag"        * ("inventory.area" * cohort_proportion)) AS StemSnag,
+  SUM("pools.SoftwoodBranchSnag"      * ("inventory.area" * cohort_proportion)) +
+  SUM("pools.HardwoodBranchSnag"      * ("inventory.area" * cohort_proportion)) AS BranchSnag,
   SUM("pools.CO2"                     * ("inventory.area" * cohort_proportion)) AS CO2,
   SUM("pools.CH4"                     * ("inventory.area" * cohort_proportion)) AS CH4,
   SUM("pools.CO"                      * ("inventory.area" * cohort_proportion)) AS CO,
   SUM("pools.NO2"                     * ("inventory.area" * cohort_proportion)) AS NO2,
-  SUM("pools.Products"                * ("inventory.area" * cohort_proportion)) AS Products,
+  SUM("pools.Products"                * ("inventory.area" * cohort_proportion)) AS Products
 FROM simulation
 -- WHERE
 GROUP BY timestep
