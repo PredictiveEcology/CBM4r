4c3a13ef0515698393ee2a599b054333a0c3dbb7
Susan Murray <murray.e.susan@gmail.com>
2026-03-25 11:42:05 -0700

    arrow_space_dataset_read_table adds col_select argument

--- R/arrow_space_dataset_read_table.R
+++ R/arrow_space_dataset_read_table.R
@@ -3,14 +3,15 @@ arrow_space_dataset_read_table <- function(
     dataset_dir = NULL,
     dataset_name,
     table_name = NULL,
+    col_select = NULL,
     dataset_path = file.path(dataset_dir, dataset_name),
     ...
 ){
 
   table_path <- file.path(dataset_path, paste0(dataset_name, if (!is.null(table_name)) paste0("-", table_name)))
 
-  data.table::as.data.table(
-    dplyr::collect(arrow::open_dataset(table_path), ...)
-  )
+  table_dataset <- arrow::open_dataset(table_path, ...)
+  if (!is.null(col_select)) table_dataset <- dplyr::select(table_dataset, col_select)
+  data.table::as.data.table(dplyr::collect(table_dataset))
 }
 
