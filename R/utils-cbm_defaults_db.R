83c6374c53555114d2793c4489a28a8539c602fa
Susan Murray <murray.e.susan@gmail.com>
2026-04-21 13:25:47 -0700

    CBM defaults db connect with read only

--- R/utils-cbm_defaults_db.R
+++ R/utils-cbm_defaults_db.R
@@ -4,7 +4,7 @@ cbmdbReadTable <- function(cbm_defaults_db, tableName, localeID = 1){
   if (length(cbm_defaults_db) == 0)  stop("cbm_defaults_db path invalid")
   if (!file.exists(cbm_defaults_db)) stop("cbm_defaults_db not found: ", cbm_defaults_db)
 
-  cbmDBcon <- RSQLite::dbConnect(RSQLite::dbDriver("SQLite"), cbm_defaults_db)
+  cbmDBcon <- RSQLite::dbConnect(RSQLite::dbDriver("SQLite"), cbm_defaults_db, flags = RSQLite::SQLITE_RO)
   on.exit(RSQLite::dbDisconnect(cbmDBcon))
 
   tbl <- data.table::as.data.table(RSQLite::dbReadTable(cbmDBcon, tableName))
