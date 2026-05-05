bbcd9e0de77bef7b370511d1ca5fc157b91aea14
Susan Murray <murray.e.susan@gmail.com>
2026-05-05 08:48:30 -0700

    class check change

--- R/cbm4_results_processor.R
+++ R/cbm4_results_processor.R
@@ -7,9 +7,7 @@
 #' @export
 cbm4_results_processor <- function(cbm4_data, views = TRUE){
 
-  if (inherits(cbm4_data, "cbm4.app.spatial.results.sql_results_processor.SQLResultsProcessor")){
-    return(cbm4_data)
-  }
+  if (!is.character(cbm4_data)) return(cbm4_data)
 
   if (length(cbm4_data) == 0)  stop("cbm4_data invalid")
   if (!file.exists(cbm4_data)) stop("cbm4_data not found: ", cbm4_data)
