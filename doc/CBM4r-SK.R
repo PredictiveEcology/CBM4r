65b2ac009cde19085084e50778c94aa1feefac8b
Susan Murray <murray.e.susan@gmail.com>
2026-05-05 20:12:09 -0700

    vignette added

---
+++ doc/CBM4r-SK.R
@@ -0,0 +1,154 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ----setup--------------------------------------------------------------------
+library(CBM4r)
+
+## ----inputs-------------------------------------------------------------------
+# Set input test data directory
+testdata <- system.file(file.path("testdata", "SK"), package = "CBM4r")
+
+# Read study area grid
+grid_rast <- do.call(terra::rast, data.table::fread(file.path(testdata, "grid_rast.csv")))
+
+# Read cohort data and cohort classifier columns
+cohorts     <- data.table::fread(file.path(testdata, "cohorts.csv"))
+classifiers <- readLines(file.path(testdata, "classifiers.txt"))
+
+# Read growth curve metadata and carbon increments
+gc_meta <- data.table::fread(file.path(testdata, "gc_meta.csv"))
+gc_incr <- data.table::fread(file.path(testdata, "gc_incr.csv"))
+
+# Read disturbance metadata and events
+dist_meta   <- data.table::fread(file.path(testdata, "dist_meta.csv"))
+dist_events <- data.table::fread(file.path(testdata, "dist_events.csv"))
+
+## ----virtualeenv--------------------------------------------------------------
+# Create CBM4 virtual environment
+cbm4_virtualenv_create("r-CBM4")
+
+# Use CBM4 virtual environment
+reticulate::use_virtualenv("r-CBM4")
+
+## ----paths--------------------------------------------------------------------
+# Set CBM4 output data directory path
+cbm4_data <- "cbm4_data"
+
+
+## ----grid_meta----------------------------------------------------------------
+# Set study area metadata
+grid_meta <- cbm4_grid_meta(
+  grid_rast       =  grid_rast,
+  admin_boundary  = "Saskatchewan",
+  eco_boundary_id = 6
+)
+
+# Alternatively: set grid metadata in an existing pixels table
+grid_meta <- data.table::data.table(
+  pixel_index     = 1:terra::ncell(grid_rast),
+  admin_boundary  = "Saskatchewan",
+  eco_boundary_id = 6
+)
+cbm4_set_grid_meta(grid_meta, grid_rast = grid_rast)
+
+## ----spinup-------------------------------------------------------------------
+# Write inventory to CBM4 data directory
+cbm4_write_inventory(
+  cbm4_data,
+  grid_meta   = grid_meta,
+  grid_rast   = grid_rast,
+  cohorts     = cohorts,
+  classifiers = classifiers
+)
+
+# Write spinup parameters to CBM4 data directory
+cbm4_write_spinup_parameters(
+  cbm4_data,
+  gc_meta     = gc_meta,
+  gc_incr     = gc_incr,
+  classifiers = classifiers
+)
+
+# Run CBM4 spinup
+cbm4_spinup(cbm4_data)
+
+
+## ----step---------------------------------------------------------------------
+# Write annual step parameters to CBM4 data directory
+cbm4_write_step_parameters(
+  cbm4_data,
+  gc_meta     = gc_meta,
+  gc_incr     = gc_incr,
+  classifiers = classifiers
+)
+
+# Write disturbances to CBM4 data directory
+cbm4_write_disturbance(
+  cbm4_data,
+  grid_meta   = grid_meta,
+  dist_meta   = dist_meta,
+  dist_events = dist_events
+)
+
+# Run annual step for year 1
+cbm4_step(cbm4_data, timestep = 1)
+
+# Run annual step for year 2
+cbm4_step(cbm4_data, timestep = 2)
+
+## ----results_processor--------------------------------------------------------
+# Get CBM4 SQLResultsProcessor
+cbm4_results <- cbm4_results_processor(cbm4_data)
+
+## ----results_totals, results = "hide"-----------------------------------------
+# Read pools totals by timestep
+cbm4_results_totals(
+  cbm4_results,
+  view_name = "composite_pool_indicators",
+  units     = "t"
+)
+cbm4_results_totals(
+  cbm4_results,
+  view_name = "composite_pool_indicators",
+  units     = "Mt"
+)
+
+# Read flux totals by timestep
+cbm4_results_totals(
+  cbm4_results,
+  view_name = "composite_flux_indicators",
+  units     = "t"
+)
+cbm4_results_totals(
+  cbm4_results,
+  view_name = "composite_flux_indicators",
+  units     = "Mt"
+)
+
+# View a list of results that can be read into a summary table
+totalOpts <- cbm4_results_totals(cbm4_results, list = TRUE)
+
+## ----results_raster, results = "hide", fig.show = "hide"----------------------
+# Read Net Primary Productivity (NPP) raster
+rastNPP <- cbm4_results_raster(
+  cbm4_results, 
+  view_name   = "spatial_composite_flux_indicators",
+  view_column = "Ecosystem Indicators - Productivity - Net Primary Productivity (NPP)",
+  timesteps   = 1:2
+)
+terra::plot(
+  rastNPP,
+  main       = c("timestep 1: NPP (tC/ha)", "timestep 2: NPP (tC/ha)"),
+  type       = "continuous",
+  col        = colorRampPalette(c("lightgreen", "darkgreen"))(10),
+  breaks     = seq(0, ceiling(max(terra::values(rastNPP), na.rm = TRUE)), length.out = 10),
+  background = "darkgrey",
+  axes       = FALSE
+)
+
+# View a list of results that can be read into a raster
+rastOpts <- cbm4_results_raster(cbm4_results, list = TRUE)
+
