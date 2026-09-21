c763ff66155f40b0f28e206d1d8106ceb2b0625f
Susan Murray <murray.e.susan@gmail.com>
2026-09-21 09:34:52 -0700

    version 1.0.1

--- R/cbm4_virtualenv_create.R
+++ R/cbm4_virtualenv_create.R
@@ -121,36 +121,13 @@ cbm4_versions <- function(version = NULL){
     "3.0.0" = list(
       python       = ">=3.12",
       gdal_win     = "https://github.com/cgohlke/geospatial-wheels/releases/download/v2025.10.25/gdal-3.11.4-cp312-cp312-win_amd64.whl",
-      cbm4         = "87e5bec" # 2026-08-13
+      cbm4         = "3.0.0",
     ),
     "2.24.9" = list(
       python       = ">=3.12",
       gdal_win     = "https://github.com/cgohlke/geospatial-wheels/releases/download/v2025.10.25/gdal-3.11.4-cp312-cp312-win_amd64.whl",
       packages     = "pandas==2.3.3",
-      cbm4         = "625f7ff" # 2026-07-17
-    ),
-    "2.24.3" = list(
-      python       = ">=3.12",
-      gdal_win     = "https://github.com/cgohlke/geospatial-wheels/releases/download/v2025.10.25/gdal-3.11.4-cp312-cp312-win_amd64.whl",
-      packages     = "pandas==2.3.3",
-      cbm4         = "063034f1b9d90e9609e5f80d37d44a3f1ff3194c", # 2026-06-25
-      arrow_space  = "8744e4c80daa327fdf7aeda604d8e755f96aa509", # 2026-06-24
-      cbmspec_cbm3 = "c13bc70a2f14e9aab614996324855701c6947913"  # 2026-06-24
-    ),
-    "2.17.10" = list(
-      python       = ">=3.12",
-      gdal_win     = "https://github.com/cgohlke/geospatial-wheels/releases/download/v2025.10.25/gdal-3.11.4-cp312-cp312-win_amd64.whl",
-      packages     = "pandas==2.3.3",
-      cbm4         = "b8f0991bcd723661fd74aec52dd0e314c7d26dbb", # 2026-03-27
-      arrow_space  = "7715ba811ef34a62ffd859d73e02c8659c4bf311", # 2026-03-27
-      cbmspec_cbm3 = "da8c70a47c9ea7163a2e99dd6703bafef9b12a4e", # 2026-03-31
-      libcbm       = "fee02dfa839fe4cee2252208d9e1e452a5e7adf3"  # 2026-01-21
-    ),
-    "2.17.9" = list(
-      python   = ">=3.12,<3.13",
-      gdal_win = "https://github.com/cgohlke/geospatial-wheels/releases/download/v2025.7.4/gdal-3.11.1-cp312-cp312-win_amd64.whl",
-      gdal     = c(min = "0", max = "3.11.3"),
-      cbm4     = "2.17.9"
+      cbm4         = "2.24.9"
     )
   )
 
